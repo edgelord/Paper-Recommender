b1af0beec5f8bc2e16164430312d39bffd4fe91a
YOUR NAMEAkshay Mishra <akmishr2@illinois.edu>
2016-05-05 16:51:06 -0500

    Implemented tar extraction

--- src/download.py
+++ src/download.py
@@ -1,10 +1,12 @@
+import os
 import wget
 import tarfile
 
 names_file  = "../resources/files.txt"
 names_file  = "../resources/download_names.txt"
 base_str = r"http://acl-arc.comp.nus.edu.sg/archives/acl-arc-160301-parscit/{0}.tgz"
-tar_dir = "../resources/tars"
+tar_dir = "../resources/tars/"
+xml_dir = "../resources/xmls/"
 
 str = r"http://acl-arc.comp.nus.edu.sg/archives/acl-arc-160301-parscit/{0}.tgz".format("A00")
 
@@ -16,3 +18,10 @@ def download_files(filename = names_file):
         print "Downloading: {0}".format(name)
         wget.download(base_str.format(name), out=tar_dir)
 
+def extract_tars(dirname = xml_dir):
+    files = os.listdir(tar_dir)
+    tars = [tar_dir+f for f in files if "tgz" in f]
+    for tar in tars:
+        tf = tarfile.open(tar)
+        tf.extractall(xml_dir)
+    return tars
